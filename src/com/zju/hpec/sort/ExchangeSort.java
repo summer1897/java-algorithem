5420e37ea683c249bd6ed019fb04eb427a86a66b
summer2016 <nihao5201314>
2016-04-11 13:13:27 +0800

    code format

--- src/com/zju/hpec/sort/ExchangeSort.java
+++ src/com/zju/hpec/sort/ExchangeSort.java
@@ -4,9 +4,16 @@ import com.zju.hpec.utils.Utils;
 
 /**
  *
- * <p>Title: ExchangeSort</p>
- * <p>Description: exchange sort mainly includes bubble sort and quick sort</p>
- * <p>Company: </p>
+ * <p>
+ * Title: ExchangeSort
+ * </p>
+ * <p>
+ * Description: exchange sort mainly includes bubble sort and quick sort
+ * </p>
+ * <p>
+ * Company:
+ * </p>
+ * 
  * @author summer
  * @date 2016年4月9日 下午7:17:17
  *
@@ -15,98 +22,106 @@ public class ExchangeSort {
 
 	/**
 	 * the general bubble sort
+	 * 
 	 * @param array
 	 */
-	public static void bubbleSort(int[] array){
+	public static void bubbleSort(int[] array) {
 		int len = array.length;
-		for(int i = 0; i < len - 1;++i){
-			for(int j = 0; j < len - i - 1;++j){
-				if(array[j] > array[j + 1]){
+		for (int i = 0; i < len - 1; ++i) {
+			for (int j = 0; j < len - i - 1; ++j) {
+				if (array[j] > array[j + 1]) {
 					Utils.swap(array, j, j + 1);
 				}
 			}
 		}
 	}
-	
+
 	/**
-	 * the improved bubble sort will end just when there exists a situation where all the element
-	 * in the array aren't swap
+	 * the improved bubble sort will end just when there exists a situation
+	 * where all the element in the array aren't swap
+	 * 
 	 * @param array
 	 */
-	public static void improveBubbleSort(int[] array){
+	public static void improveBubbleSort(int[] array) {
 		int len = array.length;
-		for(int i = 0; i < len - 1; ++i){
-			boolean isSwap =false;
-			for(int j = 0; j < len - i -1; ++j){
-				if(array[j] > array[j + 1]){
+		for (int i = 0; i < len - 1; ++i) {
+			boolean isSwap = false;
+			for (int j = 0; j < len - i - 1; ++j) {
+				if (array[j] > array[j + 1]) {
 					isSwap = true;
 					Utils.swap(array, j, j + 1);
 				}
 			}
-			//if there is no comparison in this time, directly end the cycle 
-			if(!isSwap)
+			// if there is no comparison in this time, directly end the cycle
+			if (!isSwap)
 				break;
 		}
 	}
-	
+
 	/**
-	 * quick sort can't sort the array in which there are more than two same element 
+	 * quick sort can't sort the array in which there are more than two same
+	 * element
+	 * 
 	 * @param array
 	 * @param left
 	 * @param right
 	 */
-	public static void quickSort(int[] array,int left,int right){
-		if(left < right){
-			int pivot = partition(array,left,right);
-			quickSort(array,left,pivot - 1);
-			quickSort(array,pivot + 1,right);
+	public static void quickSort(int[] array, int left, int right) {
+		if (left < right) {
+			int pivot = partition(array, left, right);
+			quickSort(array, left, pivot - 1);
+			quickSort(array, pivot + 1, right);
 		}
 	}
-	
+
 	/**
 	 * find the index position of the pivot element
+	 * 
 	 * @param array
 	 * @param left
 	 * @param right
 	 * @return
 	 */
-	private static int partition(int[] array,int left,int right){
-		//default select the left element of the array as the pivot element
+	private static int partition(int[] array, int left, int right) {
+		// default select the left element of the array as the pivot element
 		int pivot = array[left];
-//		 ++left;
-		while(left < right){
-			
-			//find the first element which less than the pivot in the right side
-			//of the array
-			while(left < right && array[right] > pivot)
+		// ++left;
+		while (left < right) {
+
+			// find the first element which less than the pivot in the right
+			// side
+			// of the array
+			while (left < right && array[right] > pivot)
 				--right;
-			
-			//find the first element which great than the pivot in the left side
-			//of the array
-			while(left < right && array[left] < pivot)
+
+			// find the first element which great than the pivot in the left
+			// side
+			// of the array
+			while (left < right && array[left] < pivot)
 				++left;
-			
-			if(left < right){
+
+			if (left < right) {
 				Utils.swap(array, left, right);
 			}
 		}
-//		Utils.print(array);
+		// Utils.print(array);
 		pivot = left;
 		return pivot;
 	}
-	
+
 	/**
 	 * test
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		int[] array = Utils.getArray();
-//		int[] array = new int[]{2,1,3,9,10,8,7,6,4,5};
+		// int[] array = new int[]{2,1,3,9,10,8,7,6,4,5};
 		Utils.print(array);
-//		bubbleSort(array);
-//		improveBubbleSort(array);
-		quickSort(array,0,array.length - 1);
+		// bubbleSort(array);
+		// improveBubbleSort(array);
+		quickSort(array, 0, array.length - 1);
 		Utils.print(array);
 	}
-	
+
 }
--- src/com/zju/hpec/sort/SelectSort.java
+++ src/com/zju/hpec/sort/SelectSort.java
@@ -6,131 +6,140 @@ import com.zju.hpec.utils.Utils;
  *
  * @Title: Select Sort
  * @Description: the select sort mainly includes directly sort and heap sort
- * @Company: 
- * @author summer
- * 2016年4月10日
+ * @Company:
+ * @author summer 2016年4月10日
  *
  */
 public class SelectSort {
 
 	/**
-	 * directly sort 
-	 * 1. Firstly,select the index of the minimum or the maximum element in the disordered array
-	 * 2. Secondly,swap the first or last element in the disordered array and 
-	 * the selected minimum or maximum element
+	 * directly sort 1. Firstly,select the index of the minimum or the maximum
+	 * element in the disordered array 2. Secondly,swap the first or last
+	 * element in the disordered array and the selected minimum or maximum
+	 * element
+	 * 
 	 * @param array
 	 */
-	public static void directSelectSort(int[] array){
+	public static void directSelectSort(int[] array) {
 		int len = array.length;
-		for(int i = 0; i < len; ++i){
-			//record the index of the minimum element in the disordered array
+		for (int i = 0; i < len; ++i) {
+			// record the index of the minimum element in the disordered array
 			int min = i;
-			for(int j = i + 1; j < len; ++j){
-				if(array[min] > array[j])
+			for (int j = i + 1; j < len; ++j) {
+				if (array[min] > array[j])
 					min = j;
 			}
-			
-			//if the index of the minimum element is not equal to the i
-			//swap the two element corresponding to the i and min,respectively
-			if(i != min)
+
+			// if the index of the minimum element is not equal to the i
+			// swap the two element corresponding to the i and min,respectively
+			if (i != min)
 				Utils.swap(array, i, min);
 		}
 	}
-	
+
 	/**
 	 * heap sort
+	 * 
 	 * @param array
 	 */
-	public static void heapSort(int[] array){
+	public static void heapSort(int[] array) {
 		int len = array.length;
-		for(int i = 0; i < array.length; ++i){
-			buildMaxHeap(array,len - i - 1);
-			//the maximum element of the array will be set to the first position
-			//of the array after building the maximum heap
+		for (int i = 0; i < array.length; ++i) {
+			buildMaxHeap(array, len - i - 1);
+			// the maximum element of the array will be set to the first
+			// position
+			// of the array after building the maximum heap
 			Utils.swap(array, 0, len - i - 1);
 		}
-//		
-//		for(int i = 0; i < len; ++i){
-		//the minimum element of the array will be set to the first position
-		//of the array after building the minimum heap
-//			buildMinHeap(array,len - i -1);
-//			Utils.swap(array, 0, len - i -1);
-//		}
+		//
+		// for(int i = 0; i < len; ++i){
+		// the minimum element of the array will be set to the first position
+		// of the array after building the minimum heap
+		// buildMinHeap(array,len - i -1);
+		// Utils.swap(array, 0, len - i -1);
+		// }
 	}
-	
+
 	/**
 	 * build max heap
+	 * 
 	 * @param array
-	 * @param last the last position of the array will be used to build heap
+	 * @param last
+	 *            the last position of the array will be used to build heap
 	 */
-	private static void buildMaxHeap(int[] array,int last){
-		for(int i = (last - 1) / 2; i >= 0; --i){
+	private static void buildMaxHeap(int[] array, int last) {
+		for (int i = (last - 1) / 2; i >= 0; --i) {
 			int k = i;
-			while(k * 2 + 1 < last){
+			while (k * 2 + 1 < last) {
 				int maxIndex = k * 2 + 1;
-				
-				//find the maximum value between the two children nodes of the current node
-				//and set the index to the maxIndex variable
-				if(maxIndex < last){
-					if(array[maxIndex] < array[maxIndex + 1])
+
+				// find the maximum value between the two children nodes of the
+				// current node
+				// and set the index to the maxIndex variable
+				if (maxIndex < last) {
+					if (array[maxIndex] < array[maxIndex + 1])
 						++maxIndex;
 				}
-				
-				//judge if the parent node is less than the child node
-				//swap the parent and the child node if the judge statement is true
-				//and then set the maxIndex value to the k variable,recursively
-				//continue that operation above,or break and return directly
-				if(array[maxIndex] > array[k]){
+
+				// judge if the parent node is less than the child node
+				// swap the parent and the child node if the judge statement is
+				// true
+				// and then set the maxIndex value to the k variable,recursively
+				// continue that operation above,or break and return directly
+				if (array[maxIndex] > array[k]) {
 					Utils.swap(array, k, maxIndex);
 					k = maxIndex;
-				}else{
+				} else {
 					break;
 				}
 			}
 		}
 	}
-	
+
 	/**
 	 * build minimum heap
+	 * 
 	 * @param array
 	 * @param last
 	 */
 	@SuppressWarnings("unused")
-	private static void buildMinHeap(int[] array,int last){
-		for(int i = (last - 1) / 2; i >= 0; --i){
+	private static void buildMinHeap(int[] array, int last) {
+		for (int i = (last - 1) / 2; i >= 0; --i) {
 			int k = i;
-			while(k * 2 + 1 < last){
+			while (k * 2 + 1 < last) {
 				int minIndex = k * 2 + 1;
-				//find the minimum value between the two children nodes of the current node
-				//and set the index to the maxIndex variable
-				if(minIndex < last){
-					if(array[minIndex] > array[minIndex + 1])
+				// find the minimum value between the two children nodes of the
+				// current node
+				// and set the index to the maxIndex variable
+				if (minIndex < last) {
+					if (array[minIndex] > array[minIndex + 1])
 						++minIndex;
 				}
-				
-				//judge if the parent node is great than the child node
-				//swap the parent and the child node if the judge statement is true
-				//and then set the maxIndex value to the k variable,recursively
-				//continue that operation above,or break and return directly
-				if(array[minIndex] < array[k]){
+
+				// judge if the parent node is great than the child node
+				// swap the parent and the child node if the judge statement is
+				// true
+				// and then set the maxIndex value to the k variable,recursively
+				// continue that operation above,or break and return directly
+				if (array[minIndex] < array[k]) {
 					Utils.swap(array, minIndex, k);
 					k = minIndex;
-				}else{
+				} else {
 					break;
 				}
 			}
 		}
 	}
-	
-	
+
 	/**
 	 * Test
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		int[] array = Utils.getArray();
 		Utils.print(array);
-//		directSelectSort(array);
+		// directSelectSort(array);
 		heapSort(array);
 		Utils.print(array);
 	}
